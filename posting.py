801e0785263811b4e3285a0bc281c150f0c87905
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-08-27 11:03:18 -0400

    time measure

--- posting.py
+++ posting.py
@@ -1,4 +1,4 @@
-from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc import Client, WordPressPost, WordPressPage
 from wordpress_xmlrpc.methods.posts import NewPost
 
 #authenticate
@@ -10,13 +10,18 @@ print "authenticating",wp_username
 wp = Client(wp_url, wp_username, wp_password)
 
 #post and activate new post
-print "posting"
-post = WordPressPost()
-post.title = 'My post'
-post.content = 'This is a wonderful blog post about XML-RPC.'
-post.post_status = 'publish'
-post.terms_names = {
-  'post_tag': ['test', 'firstpost'],
-  'category': ['Introductions', 'Tests']
-}
-wp.call(NewPost(post))
+# Checking time for post
+import time
+now = time.time()
+for i in range(10):
+	print "posting"
+	post = WordPressPost()
+	post.title = 'My post'
+	post.content = 'This is a wonderful blog post about XML-RPC.'
+	post.post_status = 'publish'
+	wp.call(NewPost(post))
+	
+print "posting done"
+print (time.time() - now)
+
+
